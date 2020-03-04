5cb32d11551af1d2ed54ecd8b1d3fd05f6cff39e
Justin <justinw13a@gmail.com>
2020-03-04 11:02:43 -0600

    Worked on gateway and messages

--- src/main/java/edu/lawrence/asteroidgame/Network/Messages/Message.java
+++ src/main/java/edu/lawrence/asteroidgame/Network/Messages/Message.java
@@ -3,16 +3,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package edu.lawrence.asteroidgame.Network;
+package edu.lawrence.asteroidgame.Network.Messages;
+
+import edu.lawrence.asteroidgame.Network.NetworkConsts;
 
 /**
  *
  * @author Justin
  */
-public class Message {
+public class Message implements NetworkConsts{
     private final int messageType;
     
-    private Message(int messageType){
+    protected Message(int messageType){
         this.messageType = messageType;
     }
 
--- src/main/java/edu/lawrence/asteroidgame/Network/NetworkConsts.java
+++ src/main/java/edu/lawrence/asteroidgame/Network/NetworkConsts.java
@@ -11,10 +11,7 @@ package edu.lawrence.asteroidgame.Network;
  */
 public interface NetworkConsts {
     //messages to be recieved
-    public static final int UPDATE_OBJECT   = 1;
-    public static final int CREATE_OBJECT   = 2;
-    public static final int DELETE_OBJECT   = 3;
-    public static final int UPDATE_PROGRESS = 4;
-    //messages to be sent
-    public static final int UPDATE_PLAYER   = 5;
+    public static final int GET_PROGRESS = 1;
+    public static final int UPDATED_PROGRESS = 2;
+    
 }
